eff3a457a72631e0061044f6fd9f12b1d25a29ab
JinChen <eric.chen.it@gmail.com>
2020-02-11 21:28:51 +1100

    add crud to group controller

--- CorePractice/Controllers/GroupController.cs
+++ CorePractice/Controllers/GroupController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CorePractice.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace CorePractice.Controllers
@@ -27,10 +28,57 @@ namespace CorePractice.Controllers
 
 
         [HttpGet]
-        public IActionResult GetUsers()
+        public IActionResult GetGroups()
         {
-            return Ok();
+            var Group = _db.Groups.OrderBy(m => m.GroupId).ToList<Group>();
+
+            return Ok(Group);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateGroup(Group group)
+        {
+            _db.Groups.Add(group);
+
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction("CreateGroup", group);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateGroup(Group group)
+        {
+            _db.Entry(group).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> DeleteGroup(int id)
+        {
+            var Group = _db.Groups.Where<Group>(m => m.GroupId == id).FirstOrDefault();
+
+            if (Group == null)
+            {
+                return NotFound();
+            }
+
+            _db.Groups.Remove(Group);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(Group);
+        }
     }
 }
